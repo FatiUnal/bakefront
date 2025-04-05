ebd219af930cc75ddfa124dcf30e55c55819b2cf
Ömer Türkay <omerturkay@Omer-MacBook-Air.local>
2025-04-05 17:22:49 +0300

    asdgasd

--- src/components/Anasayfa/teamListCard/TeamListCard.jsx
+++ src/components/Anasayfa/teamListCard/TeamListCard.jsx
@@ -1,8 +1,18 @@
 import "./TeamListCard.scss";
 function TeamlisCard({ proje }) {
+  const handleDownload = (e) => {
+    e.preventDefault(); // Linkin normal davranışını durdur
+    const link = document.createElement("a");
+    link.href = proje.images[0];
+    link.setAttribute("download", "file");
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+
   return (
     <li className="glide__slide">
-      <a href={proje.images[0]} className="card">
+      <a href={proje.images[0]} className="card" onClick={handleDownload}>
         <div className="img">
           <img src={proje.coverImage} alt="" />
         </div>
--- src/components/listCard/LİstCard.jsx
+++ src/components/listCard/LİstCard.jsx
@@ -2,9 +2,19 @@ import { Link } from "react-router-dom";
 import "./ListCard.scss";
 
 const LİstCard = ({ proje }) => {
+  const handleDownload = (e) => {
+    e.preventDefault(); // Linkin normal davranışını durdur
+    const link = document.createElement("a");
+    link.href = proje.images[0];
+    link.setAttribute("download", "file");
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+
   return (
     <div className="projeCard">
-      <a href={proje.images[0]} className="img">
+      <a href={proje.images[0]} className="img" onClick={handleDownload}>
         <img src={proje.coverImage} alt="kapakresmi" />
       </a>
       <div className="detayCard">
